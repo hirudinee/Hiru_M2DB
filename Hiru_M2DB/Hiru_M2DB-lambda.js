4992c4e39005dc2235e7d171a03c299af80168e6
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-18 17:04:26 +0530

    DB removed

--- Hiru_M2DB/Hiru_M2DB-lambda.js
+++ Hiru_M2DB/Hiru_M2DB-lambda.js
@@ -1,18 +1,6 @@
 let AWS = require('aws-sdk');
-const ddb = new AWS.DynamoDB.DocumentClient();
-exports.handler = function (event, context, callback) {
-
-	ddb.put({
-		TableName: 'HiruTest',
-		Item: { 'sample': '123' }
-	}, function (err, data) {
-		if (err) {
-			console.log('Error',err);
-		} else {
-			console.log('success',data);
-		}
-	});
+exports.handler = function(event, context, callback) {
 
 
-	callback(null, 'Successfully executed');
+	callback(null,'Successfully executed');
 }
